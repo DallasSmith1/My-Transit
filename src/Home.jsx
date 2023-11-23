7de37450c93e642293056ad248f4bcce19d7a814
DallasSmith1 <mervyndallas.smith@dcmail.ca>
2023-11-22 20:54:01 -0500

    Nav Updater
    updated the navbar title when swapping pages

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,8 @@
 function Home()
 {
+    let page = document.getElementById("currentPage")
+    page.innerText = "Home";
+
     return (
         <h2>Home</h2>
     );
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,11 +3,17 @@ import {Link} from "react-router-dom";
 function Nav() {
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-success">
-  <a className="navbar-brand" href="#">My Transit</a>
-    <div className="navbar-nav">
-      <Link to="/">Home</Link>
-      <Link to="/schedule">Schedule</Link>
-    </div>
+  <a className="navbar-brand" id="currentPage" href="#">Navbar</a>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <Link className="nav-link" to="/">Home</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/schedule">Schedule</Link>
+      </li>
+    </ul>
+  </div>
 </nav>
   );
 }
